3757edc131c27d1aff4f5af9453f41c52892bdbd
Mahendra Dash <mahendra.ku.dash@gmail.com>
2018-08-04 18:36:01 +0100

    Eclipse project created and the package are corrected.

---
+++ src/main/java/com/mdash/alloter/Alloter.java
@@ -0,0 +1,11 @@
+package com.mdash.alloter;
+
+public class Alloter {
+
+	public static void main(String[] args) {
+		System.out.println("Alloter started..");
+
+		System.out.println("Alloter stoped..");
+	}
+
+}
