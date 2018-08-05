47cc51e85f4b944772126532695977658a6b6624
Mahendra Dash <mahendra.ku.dash@gmail.com>
2018-08-05 15:55:31 +0100

    More Members added, now 22

--- src/main/java/com/mdash/alloter/MemberService.java
+++ src/main/java/com/mdash/alloter/MemberService.java
@@ -21,7 +21,19 @@ public class MemberService {
 		members.add(new Member(8, "Mahendra8", "Dash8"));
 		members.add(new Member(9, "Mahendra9", "Dash9"));
 		members.add(new Member(10, "Mahendra10", "Dash10"));
-
+		members.add(new Member(11, "Mahendra11", "Dash11"));
+		members.add(new Member(12, "Mahendra12", "Dash12"));
+		members.add(new Member(13, "Mahendra13", "Dash13"));
+		members.add(new Member(14, "Mahendra14", "Dash14"));
+		members.add(new Member(15, "Mahendra15", "Dash15"));
+		members.add(new Member(16, "Mahendra16", "Dash16"));
+		members.add(new Member(17, "Mahendra17", "Dash17"));
+		members.add(new Member(18, "Mahendra18", "Dash18"));
+		members.add(new Member(19, "Mahendra19", "Dash19"));
+		members.add(new Member(20, "Mahendra20", "Dash20"));
+		members.add(new Member(21, "Mahendra21", "Dash21"));
+		members.add(new Member(22, "Mahendra22", "Dash22"));
+		
 		return members;
 	}
 
